474cae83ba9da314eef6dce81cf3bce500480f77
may <vagrant@hellocloud-native-box>
2024-05-22 14:11:39 +0000

    update

--- tf-docker-haproxy/provider.tf
+++ tf-docker-haproxy/provider.tf
@@ -1,4 +1,13 @@
 terraform {
+  cloud {
+    organization = "HelloCloud-May" #HCP Terraform organization name
+    workspaces {
+      name = "haproxy-state"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+
   required_providers {
     docker = {
       source  = "kreuzwerker/docker" # provider source
